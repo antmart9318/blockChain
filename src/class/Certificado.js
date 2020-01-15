e78eb0d518d02373c196021b515d5aec18b2ed98
antmart9318 <antmart9318@gmail.com>
2020-01-15 18:55:06 +0100

    second commit

--- src/class/Certificado.js
+++ src/class/Certificado.js
@@ -4,5 +4,15 @@ class Certificado{
         this.id = id
         this.nombre = nombre
         this.titular = titular
+        this.cantCreditos = cantCreditos
+        this.hash = hash
+        this.fechaExpedicion = fechaExpedicion
+        this.fechaVerificacion = fechaVerificacion
+        this.estado = estado
+        this.validez = validez
+    }
+
+    certificaDatosCorrectos(){
+        
     }
 }
---
+++ src/class/Solicitud.js
@@ -0,0 +1,13 @@
+class Solicitud{
+    constructor(){
+        super(this)
+        this.id = id
+        this.fecha = fecha
+        this.nombreTitulo = nombreTitulo
+        this.hashDocumento = hashDocumento
+    }
+
+    verificarCertificado(){
+        
+    }
+}
